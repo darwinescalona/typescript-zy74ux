62c0c48b837e76d84ea45214fc8fcf68c6e43a0b
darwinescalona <darwin.escalona.trabajo@gmail.com>
2019-12-28 17:12:22 -0300

    implementacion redux basica

--- index.ts
+++ index.ts
@@ -1,47 +1,12 @@
 // Import stylesheets
 import './style.css';
-import {Action, Reducer} from './ngrx-fake/ngrx';
+import {Store, createStore} from 'redux';
 import {contadorReducer} from './contador/contador.reducer';
-import {incrementadorAction, 
-        decrementadorAction, 
-        multiplicadorAction, 
-        diviosorAction, 
-        resetAction} from './contador/contador.actions';
+import {incrementadorAction} from './contador/contador.actions';
+const store: Store = createStore(contadorReducer);
 
-// Write TypeScript code!
-const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter Dj</h1>`;
+store.subscribe(() => {
+  console.log(store.getState())
+});
 
-
-
- class Store<T> {
-   constructor(private reducer: Reducer<T>, 
-              private state: T){
-
-   }
-
-  getState(){
-    return this.state;
-  }
-
-  dispatch(action: Action){
-    this.state = this.reducer(this.state, action)
-  }
- }
-
- const store = new Store (contadorReducer, 10);
-
-console.log(store.getState());
 store.dispatch(incrementadorAction);
-store.dispatch(incrementadorAction);
-console.log(store.getState());
-
-store.dispatch(multiplicadorAction);
-console.log(store.getState());
-
-
-/*
-console.log(contadorReducer(10, decrementadorAction))
-console.log(contadorReducer(10, multiplicadorAction))
-console.log(contadorReducer(10, diviosorAction))
-console.log(contadorReducer(10, resetAction))*/
